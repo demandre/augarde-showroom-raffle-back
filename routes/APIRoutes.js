e0708203ea9fe77446a651422c2b4a203a6f0018
Joris Demandre <joris.demandre@agence-soon.fr>
2019-10-16 20:59:18 +0200

    unified router request log

--- routes/APIRoutes.js
+++ routes/APIRoutes.js
@@ -5,11 +5,15 @@ const bodyParser = require('body-parser');
 const APIWooCommerce = require('../controllers/API');
 const router = express.Router();
 const Product = require('../models/product');
+const logger = function(req, res, next) {
+    console.log(req.method + " product " + req.url);
+    next();
+};
 router.use(bodyParser.json());
+router.use(logger);
 
 router.get('/all', async (req, res, next) => {
     try {
-        console.log("get /all products");
         const productsWooCommerce = await APIWooCommerce.getAllProducts();
         return res.json(productsWooCommerce.data);
     }
@@ -21,7 +25,6 @@ router.get('/all', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
         try {
-            console.log("get " + req.params.id + " product");
             if (req.params.id) {
                 const productWooCommerce = await APIWooCommerce.getProduct(req.params.id);
                 const product            = new Product(productWooCommerce.data);
@@ -37,7 +40,6 @@ router.get('/:id', async (req, res, next) => {
 
 router.get('/watch', async (req, res, next) => {
     try {
-        console.log("watch route");
         const watches = await APIWooCommerce.getWatches();
         res.json(watches);
     }
@@ -50,7 +52,6 @@ router.get('/watch', async (req, res, next) => {
 
 router.get('/cadran', async (req, res, next) => {
     try {
-        console.log("cadran route");
         const cadrans = await APIWooCommerce.getCadrans();
         res.json(cadrans);
     }
@@ -62,7 +63,6 @@ router.get('/cadran', async (req, res, next) => {
 
 router.get('/bracelet', async (req, res, next) => {
     try {
-        console.log("bracelet route");
         const bracelets = await APIWooCommerce.getBracelets();
         res.json(bracelets);
     }
