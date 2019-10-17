25b36714a9fe5a2aa97f52d8fe79f8b770b39599
Joris Demandre <joris.demandre@agence-soon.fr>
2019-10-17 15:27:13 +0200

    clean Raffle Collection code

--- models/collections/raffleCollection.js
+++ models/collections/raffleCollection.js
@@ -12,35 +12,34 @@ class RaffleCollection {
         this.connection = await new Database();
     }
 
-     load(raffleId) {
-         return new Promise(async (resolve, reject) => {
-             if(this.connection === null) {
-                 await this.init();
-             }
-             this.collection = [];
-             let query = "";
+    load(raffleId) {
+     return new Promise(async (resolve, reject) => {
+         if(this.connection === null) {
+             await this.init();
+         }
+         this.collection = [];
+         let query = "";
 
-             if(raffleId) {
-                 query = `SELECT * FROM raffle_entity WHERE id = ${raffleId}`;
-             } else {
-                 query = `SELECT * FROM raffle_entity`;
-             }
+         if(raffleId) {
+             query = `SELECT * FROM raffle_entity WHERE id = ${raffleId}`;
+         } else {
+             query = `SELECT * FROM raffle_entity`;
+         }
 
-             this.connection.query(query, function (err, result, fields) {
-                 if (err) {
-                     console.log(err);
-                     resolve(this);
-                     return;
-                 }
-
-                 result.forEach(raffleData => {
-                     let raffle = new Raffle(raffleData);
-                     this.collection.push(raffle);
-                 });
+         this.connection.query(query, function (err, result, fields) {
+             if (err) {
+                 console.log(err);
                  resolve(this);
-             }.bind(this));
-         });
+                 return;
+             }
 
+             result.forEach(raffleData => {
+                 let raffle = new Raffle(raffleData);
+                 this.collection.push(raffle);
+             });
+             resolve(this);
+         }.bind(this));
+     });
     }
 }
 
