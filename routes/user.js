dd3e373d4cacb4a710fc447afc20fd63abd296d6
Grégory <gregory.brugnet@gmail.com>
2019-10-17 18:15:12 +0200

    utils/tokenHelper (access to verify token)

--- routes/user.js
+++ routes/user.js
@@ -4,72 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const APIWooCommerce = require('../libs/woocommerceProxy');
 const router = express.Router();
+const TokenHelper = require('../utils/tokenHelper');
 //const UserCollection = require('../models/collections/userCollection')
 router.use(bodyParser.json());
-
 let userCollection = require('../models/collections/userCollection');
-
-var getWPToken = function(req, res){
-    var options;
-    // Configure the request
-    options = {
-        url: 'http://vps676674.ovh.net/wp-json/jwt-auth/v1/token?username='+req.query.username+'&password='+req.query.passwd+'',
-        method: 'POST' 
-    }
-
-    // Start the request
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            //userCollection = new UserCollection();
-            //user_email & token
-            let bodyJSON = JSON.parse(body);
-            console.log("collection : " + bodyJSON.token);
-            userCollection.add(bodyJSON.user_email, bodyJSON.token )
-            res.send({
-               success: true,
-               message: "Successfully get the token", 
-               posts: bodyJSON
-            });
-        } else {
-             console.log(error);
-        }
-    });
-   };
-
-   var verifyToken = function(req, res){
-        var options, verified
-        // Configure the request
-        options = {
-            url: 'http://vps676674.ovh.net/wp-json/jwt-auth/v1/token/validate',
-            headers: {
-                'Authorization': 'Bearer '+ req.query.token
-            },
-            method: 'POST' 
-        }
-        // Start the request
-        request(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                verified = true
-            } 
-            else {
-                res.statusCode = (401);
-                res.send({
-                    success: false,
-                    message: "Wrong token", 
-                    posts: JSON.parse(body)
-                 });
-                
-                console.log("debug : " + response.statusCode);
-                console.log(res.statusCode);
-            }
-        });
-    };
 
 router.post('/login', function(req, res){
     getWPToken(req, res);
+    TokenHelper.getWPToken(req, res);
 });
 router.get('/login/testToken', async function(req, res){
-    verifyToken(req,res);
+    TokenHelper.verifyToken(req, res);
     const product = await APIWooCommerce.getWatches();
     console.log(userCollection);
     return res.json(product);
